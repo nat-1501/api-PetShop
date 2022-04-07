26c149733e034ef6d0bf8c519e84b03f78a64e71
nat-1501 <natalisoares90@gmail.com>
2022-04-07 16:50:14 -0300

    removendo informações

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -10,12 +10,22 @@ roteador.get('/', async (requisicao, resposta) => {
 })
 
 roteador.post('/', async (requisicao, resposta) => {
-    const dadosRecebidos = requisicao.body
+    try {
+        const dadosRecebidos = requisicao.body
     const fornecedor = new Fornecedor(dadosRecebidos) 
     await fornecedor.criar()
     resposta.send(
         JSON.stringify(fornecedor)
-    ) 
+        ) 
+    }catch (erro) {
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.menssage
+            })
+        )
+    }
+
+    
 })
 
 roteador.get('/:idFornecedor', async(requisicao, resposta) => {
@@ -49,11 +59,27 @@ roteador.put('/:idFornecedor', async (requisicao, resposta) => {
     } catch (erro) {
         resposta.send(
             JSON.stringify({
-                mensagem: erro.mensage
+                mensagem: erro.menssage
             })
         )
     }
 
+})
+
+roteador.delete('/:idFornecedor', async (requisicao, resposta) => {
+    try {
+    const id = requisicao.params.idFornecedor
+    const fornecedor = new Fornecedor({ id: id })
+    await fornecedor.carregar()
+    await fornecedor.remover()
+    resposta.end()
+    } catch (erro) {
+        resposta.send(
+            JSON.stringify({
+                mensagem: erro.menssage
+            })
+        )
+    }
 })
 
 
